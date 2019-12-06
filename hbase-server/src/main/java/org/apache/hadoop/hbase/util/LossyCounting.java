2692d0b75376c3809a59e71988410d9407e8b57a
Lars Hofhansl <larsh@apache.org>
2019-12-05 18:14:48 -0800

    HBASE-23364 HRegionServer sometimes does not shut down.
    Change-Id: Ie2bb15ff4d9d4b673ed2c5b9ef5721f4b0a14617

--- hbase-server/src/main/java/org/apache/hadoop/hbase/util/LossyCounting.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/util/LossyCounting.java
@@ -32,6 +32,7 @@ import org.apache.yetus.audience.InterfaceAudience;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.hbase.thirdparty.com.google.common.annotations.VisibleForTesting;
+import org.apache.hbase.thirdparty.com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 /**
  * LossyCounting utility, bounded data structure that maintains approximate high frequency
@@ -74,7 +75,8 @@ public class LossyCounting<T> {
     this.data = new ConcurrentHashMap<>();
     this.listener = listener;
     calculateCurrentTerm();
-    executor = Executors.newSingleThreadExecutor();
+    executor = Executors.newSingleThreadExecutor(
+      new ThreadFactoryBuilder().setDaemon(true).setNameFormat("lossy-count-%d").build());
   }
 
   LossyCounting(String name, Configuration conf) {
